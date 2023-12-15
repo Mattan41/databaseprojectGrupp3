87246857c8fd76deed2272ac6a4cffe7b117ea02
DannyRyd <106701467+DevelopedByDanny@users.noreply.github.com>
2023-12-15 14:56:58 +0100

    moon sql added to schema.sql

--- jpa/src/main/resources/META-INF/schema.sql
+++ jpa/src/main/resources/META-INF/schema.sql
@@ -1,19 +1,22 @@
-CREATE TABLE planet (
-                         planetId INT AUTO_INCREMENT,
-                         planetName VARCHAR(255),
-                         planetSize INT,
-                         planetType VARCHAR(255),
-                         planetMid INT,
-                         planetSid INT,
-                         PRIMARY KEY (planetId),
-                         FOREIGN KEY (planetMid) REFERENCES moon(planetMid),
+CREATE TABLE planet
+(
+    planetId   INT AUTO_INCREMENT,
+    planetName VARCHAR(255),
+    planetSize INT,
+    planetType VARCHAR(255),
+    planetMid  INT,
+    planetSid  INT,
+    PRIMARY KEY (planetId),
+    FOREIGN KEY (planetMid) REFERENCES moon (planetMid),
 
 );
 
-CREATE TABLE moon(
-                     moonId INT NOT NULL AUTO_INCREMENT,
-                     name varchar(100) not null,
-                     size DOUBLE,
-                     planetId int not null,
-                     FOREIGN KEY (planetId) references planet
+CREATE TABLE moon
+(
+    moonId   INT          NOT NULL AUTO_INCREMENT,
+    name     varchar(100) not null,
+    size DOUBLE,
+    planetId int          not null,
+    PRIMARY KEY (moonId),
+    foreign key (planetId) references planet(planetId)
 );
