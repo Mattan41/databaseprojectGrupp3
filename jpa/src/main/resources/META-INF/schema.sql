5c166d3f557fc8d251cd992c27f84c95aa521a33
maxerkmar <max.erkmar@iths.se>
2023-12-15 14:34:38 +0100

    lade till schema.sql

---
+++ jpa/src/main/resources/META-INF/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE planet (
+                         planetId INT AUTO_INCREMENT,
+                         planetName VARCHAR(255),
+                         planetSize INT,
+                         planetType VARCHAR(255),
+                         planetMid INT,
+                         planetSid INT,
+                         PRIMARY KEY (planetId),
+                         FOREIGN KEY (planetMid) REFERENCES moon(planetMid),
+                         FOREIGN KEY (planetSid) REFERENCES solarSystem(planetSid)
+);
+
