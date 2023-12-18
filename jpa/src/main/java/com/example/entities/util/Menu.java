d45240c4979f437598f114db111a484613552bbe
DannyRyd <106701467+DevelopedByDanny@users.noreply.github.com>
2023-12-18 11:05:55 +0100

    menu and input class

---
+++ jpa/src/main/java/com/example/entities/util/Menu.java
@@ -0,0 +1,44 @@
+package com.example.entities.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private final String name;
+    private final List<MenuItem> menuItems = new ArrayList<>();
+
+    public Menu(String name) {
+        this.name = name;
+
+    }
+
+    public void addMenuItem(String description, Runnable action) {
+        menuItems.add(new MenuItem(description, action));
+    }
+
+    public void displayMenu() {
+        while (true) {
+            System.out.println("\n" + name.toUpperCase());
+            System.out.println("Options:");
+            for (int i = 0; i < menuItems.size(); i++) {
+                System.out.println((i + 1) + ". " + menuItems.get(i).description());
+            }
+            System.out.println("0. Exit");
+
+            int choice = InputReader.inputInt("Choose an option: ");
+
+            if (choice == 0) {
+                break;
+            }
+
+            if (choice > 0 && choice <= menuItems.size()) {
+                menuItems.get(choice - 1).action().run();
+            } else {
+                System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private record MenuItem(String description, Runnable action) {
+    }
+}
