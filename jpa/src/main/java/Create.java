3e83f0402a7fa514a3146e7585d0a842245bd61e
Mattan41 <Mats.kruskopf@gmail.com>
2023-12-19 09:44:02 +0100

    add create moon method, added planetID to moon

--- jpa/src/main/java/Create.java
+++ jpa/src/main/java/Create.java
@@ -1,3 +1,4 @@
+import com.example.entities.Moon;
 import com.example.entities.Planet;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -46,6 +47,43 @@ public class Create {
         em.persist(planet);
         transaction.commit();
         em.close();
-        ;
+
+    }
+
+    public static void insertMoon() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter moon to add: ");
+        String moonName = scanner.nextLine();
+        System.out.print("Enter moonSize: ");
+        Double moonSize = Double.valueOf(scanner.nextLine());
+        System.out.print("Enter planetId: ");
+        Integer planetId = Integer.parseInt(scanner.nextLine());
+
+        // Validate user input
+        if (moonName == null || moonName.isEmpty()) {
+            System.out.println("Invalid input.");
+            return;
+        }
+
+        if (moonSize <= 0 ) {
+            System.out.println("Invalid input.");
+            return;
+        }
+
+
+        Moon moon = new Moon();
+        moon.setName(moonName);
+        moon.setSize(moonSize);
+        moon.setPlanetId(planetId);
+
+        var transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(moon);
+        transaction.commit();
+        em.close();
+
     }
 }
+
+
