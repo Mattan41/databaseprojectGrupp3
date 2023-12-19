1c2f268fd61cb689f6b59edc4935f0c8effa6acb
hannastigland <hanna.stigland@iths.se>
2023-12-19 11:11:20 +0100

    added crud sql queries in data.sql

--- jpa/src/main/resources/META-INF/data.sql
+++ jpa/src/main/resources/META-INF/data.sql
@@ -1,5 +1,7 @@
+-- create
 -- add solarSystem
 INSERT INTO SolarSystem (SolarSystemName, GalaxyName) VALUES ('Solar System', 'Milky Way');
+
 -- add planets
 INSERT INTO planet (planetName, planetSize, planetType, SolarSystemId) VALUES ('Mercury', 4879, 'Terrestrial', 1);
 INSERT INTO planet (planetName, planetSize, planetType, SolarSystemId) VALUES ('Venus', 12104, 'Terrestrial', 1);
@@ -10,24 +12,15 @@ INSERT INTO planet (planetName, planetSize, planetType, SolarSystemId) VALUES ('
 INSERT INTO planet (planetName, planetSize, planetType, SolarSystemId) VALUES ('Uranus', 50724, 'Ice Giant', 1);
 INSERT INTO planet (planetName, planetSize, planetType, SolarSystemId) VALUES ('Neptune', 49244, 'Ice Giant', 1);
 
-
-
 -- add moon
--- Moons of Earth
-INSERT INTO moon (name, size, planetId) VALUES ('Moon', 3474.2, 3); -- Assuming Earth's ID is 3
-
--- Moons of Mars
-INSERT INTO moon (name, size, planetId) VALUES ('Phobos', 22.4, 4); -- Assuming Mars' ID is 4
+INSERT INTO moon (name, size, planetId) VALUES ('Moon', 3474.2, 3);
+INSERT INTO moon (name, size, planetId) VALUES ('Phobos', 22.4, 4);
 INSERT INTO moon (name, size, planetId) VALUES ('Deimos', 12.4, 4);
-
--- Moons of Jupiter (only listing 4 of many)
-INSERT INTO moon (name, size, planetId) VALUES ('Io', 3643.2, 5); -- Assuming Jupiter's ID is 5
+INSERT INTO moon (name, size, planetId) VALUES ('Io', 3643.2, 5);
 INSERT INTO moon (name, size, planetId) VALUES ('Europa', 3121.6, 5);
 INSERT INTO moon (name, size, planetId) VALUES ('Ganymede', 5268.2, 5);
 INSERT INTO moon (name, size, planetId) VALUES ('Callisto', 4820.6, 5);
-
--- Moons of Saturn (only listing 2 of many)
-INSERT INTO moon (name, size, planetId) VALUES ('Titan', 5151.8, 6); -- Assuming Saturn's ID is 6
+INSERT INTO moon (name, size, planetId) VALUES ('Titan', 5151.8, 6);
 INSERT INTO moon (name, size, planetId) VALUES ('Rhea', 1527.6, 6);
 
 -- add students
@@ -40,4 +33,31 @@ INSERT INTO student (studentSocialSecNum, studentName, studentAge, totResult) VA
 INSERT INTO test (testName, testScore, studentTestId) VALUES ('Physics', 89.5, 2);
 INSERT INTO test (testName, testScore, studentTestId) VALUES ('Chemistry', 91.2, 3);
 INSERT INTO test (testName, testScore, studentTestId) VALUES ('Mathematics', 95.0, 1);
-INSERT INTO test (testName, testScore, studentTestId) VALUES ('planet size', 100.0, 4)
+INSERT INTO test (testName, testScore, studentTestId) VALUES ('planet size', 100.0, 4);
+
+-- read
+SELECT * FROM planet;
+SELECT * FROM moon;
+SELECT * FROM moon WHERE moonId = 4;
+SELECT * FROM student WHERE totResult > 89;
+SELECT planet.planetName, moon.name AS name
+FROM planet
+INNER JOIN moon ON planet.planetId = moon.planetId
+WHERE planet.planetId = 5;
+
+-- update
+UPDATE moon SET moon.size = 23.4 WHERE moonId = 2;
+UPDATE planet SET planet.planetType = 'Big Gas Giant' WHERE planetName = 'Jupiter';
+
+-- delete
+DELETE FROM planet WHERE planetName = 'Test';
+DELETE FROM moon WHERE size < 10;
+
+-- statistics
+-- average moon size
+SELECT AVG(moon.size) FROM moon;
+-- smallest planet
+SELECT planetName, planetSize
+FROM planet
+ORDER BY planetSize ASC
+LIMIT 1;
