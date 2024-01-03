7e9db594c4f4cac9de048167304dfb6f6c01c9e2
DannyRyd <106701467+DevelopedByDanny@users.noreply.github.com>
2024-01-03 11:03:03 +0100

    removed scanner from main

--- jpa/src/main/java/com/example/Main.java
+++ jpa/src/main/java/com/example/Main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         var planetDao = new PlanetDao();
